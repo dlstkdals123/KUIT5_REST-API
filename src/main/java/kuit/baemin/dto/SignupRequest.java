ebd827261baa8fa4f98fed395a995fcf0304d87d
hamhyeongju <gudwn357@gmail.com>
2025-05-24 20:50:52 +0900

    json 생성자 매핑

--- src/main/java/kuit/baemin/dto/SignupRequest.java
+++ src/main/java/kuit/baemin/dto/SignupRequest.java
@@ -1,11 +1,20 @@
 package kuit.baemin.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
+//@Setter
 public class SignupRequest {
+
+    @JsonCreator
+    public SignupRequest(@JsonProperty("email") String email,
+                         @JsonProperty("password") String password) {
+        this.email = email;
+        this.password = password;
+    }
 
     private String email;
     private String password;
