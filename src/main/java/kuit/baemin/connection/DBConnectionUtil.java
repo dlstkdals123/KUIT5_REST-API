6725ac84adf873a5bc0403b12a9031fdc3f26ac6
hamhyeongju <gudwn357@gmail.com>
2025-05-25 01:28:19 +0900

    드라이브 매니저를 통한 커넥션 획득

---
+++ src/main/java/kuit/baemin/connection/DBConnectionUtil.java
@@ -0,0 +1,29 @@
+package kuit.baemin.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
+public class DBConnectionUtil {
+
+    @Value("${spring.datasource.url}")
+    private static String url;
+    @Value("${spring.datasource.username}")
+    private static String username;
+    @Value("${spring.datasource.password}")
+    private static String password;
+
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            log.info("get connection={}, class={}", connection, connection.getClass());
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
